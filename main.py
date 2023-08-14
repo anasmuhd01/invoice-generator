0461fdf51e4640c0d61c79b51ae24ff77373e6b7
Anas Muhammed <ansmuhd01@gmail.com>
2023-08-14 10:45:31 +0530

    pdf created with data and text added to pdf

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
-
+from fpdf import FPDF
+from pathlib import Path
 filepaths = glob.glob("invoice/*.xlsx")
 
 for filepath in filepaths:
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf.add_page()
+
+    raw_filename = Path(filepath).stem
+    filename = raw_filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice no: {filename}")
+    pdf.output(f"PDFs/{raw_filename}.pdf")
